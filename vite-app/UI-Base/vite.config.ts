7a2295b4a4ecc6cbd1c95f5579dd2f11f5d52b68
CongTraanApollogix <frontend_dev@apollogix.net>
2024-10-01 17:03:29 +0700

    config deloy vercel

--- vite-app/UI-Base/vite.config.ts
+++ vite-app/UI-Base/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3001, // Đảm bảo Vite không trùng port với Next.js
+  },
+  build: {
+    outDir: "out", // Đầu ra khi build Vite
+  },
+});
