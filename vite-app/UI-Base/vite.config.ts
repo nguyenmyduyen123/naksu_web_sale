711debdc31c9cf83765996a775175fdb9eefb61b
CongTraanApollogix <frontend_dev@apollogix.net>
2024-10-01 17:12:43 +0700

    add code

--- vite-app/UI-Base/vite.config.ts
+++ vite-app/UI-Base/vite.config.ts
@@ -4,10 +4,8 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3001, // Đảm bảo Vite không trùng port với Next.js
-  },
   build: {
-    outDir: "out", // Đầu ra khi build Vite
+    outDir: "public/assets", // Đưa các tệp đã build vào thư mục public của Next.js
+    emptyOutDir: true, // Dọn sạch thư mục trước khi build
   },
 });
